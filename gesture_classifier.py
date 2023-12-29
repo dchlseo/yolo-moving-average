5b8ef6ffa4eaea853d14b526d2515838ed40977b
SDC <dchlseo@gmail.com>
2023-12-29 20:56:09 +0900

    Update main code

--- gesture_classifier.py
+++ gesture_classifier.py
@@ -1,6 +1,7 @@
+from src.moving_average import MovingAverageFilter, class_inference_and_smoothing
 import os
 import re
-from src.video_capture import extract_frames_from_videos
+from src.video_capture import extract_frames_from_videos, save_extracted_frames
 from ultralytics import YOLO
 
 
@@ -26,25 +27,43 @@ for video_path, frames in frames_dict.items():
     if len(frames) != num_frames_per_video:
         print(f"Warning: Number of frames extracted ({len(frames)}) does not match the specified value ({num_frames_per_video}).")
 
+## UNCOMMENT BELOW IF YOU WISH TO SAVE THE FRAMES.
+# # Save the extracted frames as images
+# save_extracted_frames(frames_dict, output_dir)
+
+# # Generate a list of saved frame filenames
+# image_file_names = [f for f in os.listdir(output_dir) if f.endswith('.jpg')]
+
+# # Sort image files if needed (assuming they are named in a sortable manner)
+# image_file_names.sort()
+
+# print(f'Imported {len(image_file_names)} files.')
+
 # Import model
 model = YOLO('model/weights/best.pt')
 names = model.model.names
 print('class names: ', names)
 
+# Instantiate the moving average filter
+ma_filter = MovingAverageFilter(window_size=10)
+
+# Cache for storing results
+cached_results = {}
+
+# Check if using saved frame images or direct frame data
+using_saved_images = 'image_file_names' in locals()
+
+if using_saved_images:
+    for image_file in image_file_names:
+        image_path = os.path.join(output_dir, image_file)
+        result = class_inference_and_smoothing(image_path, model, ma_filter)
+        cached_results[image_file] = result
+else:
+    # Perform model inference directly on extracted frames
+    for video_path, frames in frames_dict.items():
+        for idx, frame in enumerate(frames):
+            result = class_inference_and_smoothing(frame, model, ma_filter)
+            cached_results[f'{os.path.basename(video_path)}_frame_{idx:03d}'] = result
 
 
-
-
-
-
-
-
-# # Function to extract number from filename for sorting
-# def extract_number(filename):
-#     match = re.search(r'(\d+)', filename)
-#     return int(match.group(0)) if match else 0
-
-# image_files = sorted([f for f in os.listdir(data_dir) if f.endswith('.jpg')], key=extract_number)
-
-# print(f'imported {len(image_files)} files.')
-
+# Plotting results
